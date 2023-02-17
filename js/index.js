1665833a7ff5a810d6b4a8718dcd04de85f6a393
DashTfied <htanzir13@gmail.com>
2023-02-18 00:12:05 +0600

    added validation/alert for inputfield functions card 1,2

--- js/index.js
+++ js/index.js
@@ -6,6 +6,14 @@ document.getElementById('btn-triangle').addEventListener('click', function(){
     const shapeName = document.getElementById('name-triangle').innerText;
     const shapeInputField = document.getElementById('input-1').value;
     const shapeInputField2 = document.getElementById('input-2').value;
+    if (
+        shapeInputField == "" ||
+        shapeInputField2 == "" ||
+        shapeInputField <= 0 ||
+        shapeInputField2 <= 0
+      ) {
+        return alert("please enter any valid number");
+      }
     const areaCalculation = 0.5 * parseFloat(shapeInputField) * parseFloat(shapeInputField2);
     
 
@@ -18,6 +26,14 @@ document.getElementById('btn-rectangle').addEventListener('click', function(){
     const shapeName = document.getElementById('name-rectangle').innerText;
     const shapeInputField3 = document.getElementById('input-3').value;
     const shapeInputField4 = document.getElementById('input-4').value;
+    if (
+        shapeInputField3 == "" ||
+        shapeInputField4 == "" ||
+        shapeInputField3 <= 0 ||
+        shapeInputField4 <= 0
+      ) {
+        return alert("please enter any valid number");
+      }
     const areaCalculation = parseFloat(shapeInputField3) * parseFloat(shapeInputField4);
     
 
@@ -43,5 +59,14 @@ document.getElementById("btn-rhombus").addEventListener('click', function(){
     const paraBase = getTextElementValueById('rhombus-d1')
     const paraHeight = getTextElementValueById('rhombus-d2')
     const areaCalculation = paraBase * paraHeight;
+    displayData(serial, shapeName, areaCalculation);
+})
+// card 5
+document.getElementById("btn-pentagon").addEventListener('click', function(){
+    serial += 1;
+    const shapeName = document.getElementById('title-pentagon').innerText;
+    const paraBase = getTextElementValueById('penta-p')
+    const paraHeight = getTextElementValueById('penta-b')
+    const areaCalculation = 0.5 * paraBase * paraHeight;
     displayData(serial, shapeName, areaCalculation);
 })
