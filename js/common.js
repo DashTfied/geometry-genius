e10e26ea4f27dc841a6a719915a86f90873156d0
DashTfied <htanzir13@gmail.com>
2023-02-17 22:48:01 +0600

    2nd car calculation

--- js/common.js
+++ js/common.js
@@ -1,6 +1,17 @@
 
 // function for calculation area data display
 
-function displayData(){
+function displayData(serial, name, result, btn){
     const container = document.getElementById('table-container');
+    const tr = document.createElement("tr");
+    tr.innerHTML = `
+    <td>${serial}</td>
+    <td>${name}</td>
+    <td>${result}</td>
+    <button class="btn btn-sm">Convert to m2</button>
+    </td>
+    `;
+    container.appendChild(tr);
 }
+
+// common function for cards
--- js/index.js
+++ js/index.js
@@ -1,9 +1,26 @@
 // calculation starts
 // card 1
+let serial = 0;
 document.getElementById('btn-triangle').addEventListener('click', function(){
+    serial +=1
     const shapeName = document.getElementById('nam-triangle').innerText;
     const shapeInputField = document.getElementById('input-1').value;
     const shapeInputField2 = document.getElementById('input-2').value;
     const areaCalculation = 0.5 * parseFloat(shapeInputField) * parseFloat(shapeInputField2);
-    console.log(shapeName, shapeInputField, shapeInputField2, areaCalculation)
+    
+
+    displayData(serial, shapeName, areaCalculation)
 })
+
+// card 2
+document.getElementById('btn-rectangle').addEventListener('click', function(){
+    serial +=1
+    const shapeName = document.getElementById('name-rectangle').innerText;
+    const shapeInputField3 = document.getElementById('input-3').value;
+    const shapeInputField4 = document.getElementById('input-4').value;
+    const areaCalculation = parseFloat(shapeInputField3) * parseFloat(shapeInputField4);
+    
+
+    displayData(serial, shapeName, areaCalculation)
+})
+// card 3
