4612e9b1452816616b6ccaccec2918fdbd6047cc
DashTfied <htanzir13@gmail.com>
2023-02-17 22:16:25 +0600

    first card calculation

---
+++ js/common.js
@@ -0,0 +1,6 @@
+
+// function for calculation area data display
+
+function displayData(){
+    const container = document.getElementById('table-container');
+}
--- js/index.js
+++ js/index.js
@@ -1,1 +1,9 @@
-console.log("js file check")
+// calculation starts
+// card 1
+document.getElementById('btn-triangle').addEventListener('click', function(){
+    const shapeName = document.getElementById('nam-triangle').innerText;
+    const shapeInputField = document.getElementById('input-1').value;
+    const shapeInputField2 = document.getElementById('input-2').value;
+    const areaCalculation = 0.5 * parseFloat(shapeInputField) * parseFloat(shapeInputField2);
+    console.log(shapeName, shapeInputField, shapeInputField2, areaCalculation)
+})
